60132a7480083e409d3d8c14ee5d5b2e26073656
Johan Steen <pastor.steen@gmail.com>
2023-03-20 09:09:08 +0100

    Add config and package files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['./app/main.js', './styles/main.scss'],
+  output: {
+    path: path.resolve(__dirname, 'public/compiled'),
+    filename: '[name].js',
+  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: "[name].css",
+  })],  
+  module: {
+    rules: [
+        {
+            test: /\.js?$/,
+            loader: "babel-loader",
+            exclude: /node_modules/
+        },
+        {
+          test: /\.(s(a|c)ss)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+          ],
+        }        
+      ] 
+    }
+};
