be2dcbfcb51508b016410786aa61d6f3510e4eaf
Johan Steen <pastor.steen@gmail.com>
2023-06-04 23:16:23 +0200

    update configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     editor: ['./app/main.js', './styles/editor/editor-main.scss'],
     reddit: ['./app/main.js', './styles/reddit/reddit-main.scss'],
@@ -36,4 +37,3 @@ module.exports = {
       ]
     }
 }
-
